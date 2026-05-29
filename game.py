c0b51057f9804b6fd17d4f2b99636e6944352c79
Lev Zurcher <lazurcher5@gmail.com>
2026-05-29 16:51:11 -0600

    Update game.py
    Added names in the tuplets for ships

--- game.py
+++ game.py
@@ -20,11 +20,11 @@ def buildBoard():
 
 def randomizeBoard(matrix):
     localMatrix = matrix
-    ships = [(5, 'C'), #Carrier
-             (4, 'B'), #Battleship
-             (3, 'R'), #Cruiser
-             (2, 'S'), #Submarine
-             (2, 'D')] #Destroyer
+    ships = [("carrier",    (5, 'C')),  #Carrier
+             ("battleship", (4, 'B')),  #Battleship
+             ("cruiser",    (3, 'R')),  #Cruiser
+             ("submarine",  (2, 'S')),  #Submarine
+             ("destroyer",  (2, 'D'))]  #Destroyer
 
 
     for ship in ships:
@@ -36,42 +36,46 @@ def placeShip(shipType, matrix):
     localMatrix = matrix
     placed = False
     canPlace = True
-    matrixLimit = shipType[0]
+    matrixLimit = shipType[1][0]
     horiz = random.choice([True, False])
     row = 0
     col = 0
-    print(f"initiliazed variables for placeShip({shipType[1]})")
+    #print(f"initiliazed variables for {shipType[0]}")
 
 
 
     while not placed:
+        loopCount = 1
         if horiz:
             row = random.randint(0,7)
             col = random.randint(0, 7 - matrixLimit)
         else:
             row = random.randint(0, 7 - matrixLimit)
             col = random.randint(0,7)
-        print("Placing ships...")
+        #print("Placing ships...")
         
         canPlace = True
         for i in range(matrixLimit):
             if horiz:
                 if localMatrix[row][col + i] != '-':
                     canPlace = False
+                    loopCount += 1
                     break
             else:
                 if localMatrix[row + i][col] != '-':
                     canPlace = False
+                    loopCount += 1
                     break
         if canPlace:
             for i in range(matrixLimit):
                 if horiz:
-                    localMatrix[row][col + i] = shipType[1]
+                    localMatrix[row][col + i] = shipType[1][1]
                 else:
-                    localMatrix[row + i][col] = shipType[1]
+                    localMatrix[row + i][col] = shipType[1][1]
             placed = True
-            print(f"Ship placed! ({shipType[1]})")
-        print(f"Trying to place ship: {shipType[1]}...")
+            #print(f"Ship placed! ({shipType[0]})")
+        #print(f"Trying to place ship: {shipType[0]}...")
+        #print(f"Loop count: {loopCount}")
     return localMatrix
 
 def displayBoard(board):
